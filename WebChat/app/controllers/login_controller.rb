f62b6073826a41e07be8f47d431779c0d8644d96
Bigbubble <bigbubble@bigmir.net>
2016-11-19 11:19:16 +0200

    New commit

--- WebChat/app/controllers/login_controller.rb
+++ WebChat/app/controllers/login_controller.rb
@@ -5,13 +5,21 @@ class LoginController < ApplicationController
   def login_verify
     @chat_user = ChatUser.where(:login => params['login'],
                                 :password => params['password']).first
-
     if @chat_user == nil
       redirect_to :root
     else
-
-    session[:login] = @chat_user.login
-    redirect_to :root
-    end
+      session[:login] = @chat_user.login
+      if params[:isadmin] == '1'
+        role_status = @chat_user.role.role
+        if role_status == 'admin'
+          session[:admin] = true
+          redirect_to admin_index_path
+        else
+          redirect_to :root
+        end
+      else
+        redirect_to :root
+      end
     end
   end
+end
